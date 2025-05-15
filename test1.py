931c0c19c199ab39457afab537cda97764345145
nouf <noufalrashdii@outlook.com>
2025-05-15 10:53:12 +0400

    test

---
+++ test1.py
@@ -0,0 +1,20 @@
+# Declaration
+FIRST_NUMBER = 100  # Upper Case - Case Sensitive
+first_number = 0  # Lower Case
+
+# Constant Variable
+TEAM_NAME = "Brain & Bytes"
+
+second_number = 0
+result = 0
+
+# Take Input
+first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
+second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
+
+# Process the Input 
+result = first_number + second_number
+
+# Output
+print("Result is: ", result, "!!!")
+print("noshii")
